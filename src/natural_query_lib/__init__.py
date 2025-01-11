bc7e9ed5926105cd7828ce5adb8c93a61fcbac15
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2025-01-11 16:31:07 -0600

    Updating some files

--- src/natural_query_lib/__init__.py
+++ src/natural_query_lib/__init__.py
@@ -1,5 +1,5 @@
-from src.natural_query_lib.enums import QueryType, JoinType
-from src.natural_query_lib.executor import QueryExecutor
-from src.natural_query_lib.query_builder import QueryBuilder
+from .enums import QueryType, JoinType
+from .executor import QueryExecutor
+from .query_builder import QueryBuilder
 
 __all__ = ["QueryBuilder", "QueryType", "JoinType", "QueryExecutor"]
